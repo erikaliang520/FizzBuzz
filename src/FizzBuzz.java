b3dfcbc11e10817d7cbe47c2422ade7ca59ce5f8
erikaliang <erika.liang@mail.utoronto.ca>
2023-09-11 14:46:19 -0400

    commit 1

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -30,5 +30,37 @@ class FizzBuzz {
 
             }
         }
+
+        int i = 1;
+        while (i <= 100) {
+
+            doFizzBuzz(i);
+            i += 1;
+        }
+
+    }
+
+    public static void doFizzBuzz(int i) {
+
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
+        }
     }
 }
